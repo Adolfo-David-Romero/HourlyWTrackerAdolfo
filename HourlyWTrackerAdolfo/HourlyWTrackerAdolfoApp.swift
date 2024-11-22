4d5e0d336ec596ed6949390f69cc17fd5fff9550
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-11-22 10:35:47 -0500

    Initial Commit

---
+++ HourlyWTrackerAdolfo/HourlyWTrackerAdolfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerAdolfoApp.swift
+//  HourlyWTrackerAdolfo
+//
+//  Created by David Romero on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct HourlyWTrackerAdolfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
