48e2ea18d409bfff0b24a29e910fb8ef3e6f914e
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2024-12-13 21:26:11 -0500

    Tested on sim. Seems to work as of now using URL session and component views

---
+++ HourlyWTrackerAdolfo/View/HourlyForecastComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  HourlyForecastView.swift
+//  HourlyWTrackerAdolfo
+//
+//  Created by David Romero on 2024-12-13.
+//
+
+import SwiftUI
+
+struct HourlyForecastComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HourlyForecastComponentView()
+}
